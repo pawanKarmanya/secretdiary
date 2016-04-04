8d091e2905e9f430bebf2aa9b4463f4cc2468584
pawan <pawankumar.s@karmanya.co.in>
2016-04-04 18:47:13 +0530

    secret diary deleted unwanted folder

--- secretdiary_01042016/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$link=mysqli_connect("localhost","dbuser","123","form");
-?>
-
-
--- secretdiary_01042016/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ('connect.php');
-session_start();
-$session=$_SESSION["id"];
-$textarea=$_POST["diary"];
-$query="update diary set diarydata='$textarea' where sessionid='$session'";
-mysqli_query($link,$query);
-?>
